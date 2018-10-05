f19f25edd25c853e2e177c477b47be1966b438de
Shun Lung Chen <slchen@mobisocial.us>
2018-10-05 14:48:21 +0800

    Initial Commit

---
+++ MeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeMe1.0
+//
+//  Created by slchen on 2018/10/5.
+//  Copyright © 2018 slchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
